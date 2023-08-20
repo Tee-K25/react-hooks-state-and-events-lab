58c8b4a3f1b916a546223b1c482bf669d640bb70
Tony <tony.kiplagat@student.moringaschool.com>
2023-08-20 16:19:30 +0300

    Done with state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isIn, setIsIn] = useState("");
+  function handleClick() {
+    setIsIn("in-cart");
+  }
   return (
-    <li className="">
+    <li className={isIn}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {isIn ? "Remove from cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
